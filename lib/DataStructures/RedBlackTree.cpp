eb7fc7cb671398b9d92b31308cbd57c4262c8d18
Beloin Sena <beloin.rodrigues@gmail.com>
2025-05-04 18:08:45 -0300

    WIP - Created left rotate and right rotate

--- lib/DataStructures/RedBlackTree.cpp
+++ lib/DataStructures/RedBlackTree.cpp
@@ -66,7 +66,11 @@ template <typename T> void RedBlackTree<T>::fixTree(Node<T> *node) {
     fixTree(node->parent);
   } else {
     if (isRight(node)) {
+      leftRotate(node->parent);
     } else {
+      // TODO: Color it
+      auto grandfather = node->parent->parent;
+      rightRotate(grandfather);
     }
   }
 }
@@ -75,18 +79,46 @@ template <typename T> bool RedBlackTree<T>::isLeft(Node<T> *node) {
   return node->parent->left == node;
 }
 
-// TODO: Wronnnngg u dumbasssss
 template <typename T> void RedBlackTree<T>::leftRotate(Node<T> *node) {
-  auto parent = node->parent;
-  parent = node->right;
-  node->right = nullptr;
-  parent->left = node;
+  auto right = node->right;
+  node->right = right->left;
+
+  if (right->left != nullptr) {
+    right->left->parent = node;
+  }
+
+  right->parent = node->parent;
+
+  if (node->parent == nullptr) { // Root
+    root = right;
+  } else if (isLeft(node)) {
+    node->parent->left = right;
+  } else {
+    node->parent->right = right;
+  }
+
+  right->left = node;
+  node->parent = right;
 }
 
 template <typename T> void RedBlackTree<T>::rightRotate(Node<T> *node) {
-  auto parent = node->parent;
-  parent = node->left;
-  node->left = nullptr;
-  parent->right = node;
-}
+  auto left = node->left;
+  node->left = left->right;
 
+  if (left->right != nullptr) {
+    left->right->parent = node;
+  }
+
+  left->parent = node->parent;
+
+  if (node->parent == nullptr) { // Root
+    root = left;
+  } else if (isLeft(node)) {
+    node->parent->left = left;
+  } else {
+    node->parent->right = left;
+  }
+
+  left->right = node;
+  node->parent = left;
+}
